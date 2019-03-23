4a830064d5c4cde60f82f62be335ed698ef40417
MLiberati <marjorie.liberati@uconn.edu>
2019-03-23 11:00:39 -0400

    Update Stabilize_2b_VisuallyTrackFrontier_eachGeneration.R

--- Stabilization/Stabilize_2b_VisuallyTrackFrontier_eachGeneration.R
+++ Stabilization/Stabilize_2b_VisuallyTrackFrontier_eachGeneration.R
@@ -1,3 +1,9 @@
+# Visualize changes in solution set across generations from 'NSGAII_forallCFAs' results
+# Liberati et al. (2019)
+# Title: "Addressing ecological, economic, and social tradeoffs of refuge expansion in a constrained landscape"
+# Journal: Landscape Ecology
+# DOI: 10.1007/s10980-019-00798-8
+
 remove(list=ls())
 
 ## R Libraries ##
