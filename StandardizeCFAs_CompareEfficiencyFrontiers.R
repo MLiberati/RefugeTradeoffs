f3934cd7f401cc333fa79362f12c349c9f7bb7b5
MLiberati <marjorie.liberati@uconn.edu>
2019-03-23 10:54:14 -0400

    Update StandardizeCFAs_CompareEfficiencyFrontiers.R

--- StandardizeCFAs_CompareEfficiencyFrontiers.R
+++ StandardizeCFAs_CompareEfficiencyFrontiers.R
@@ -1,3 +1,8 @@
+# Produce Figures 3 in Liberati et al. (2019)
+# Title: "Addressing ecological, economic, and social tradeoffs of refuge expansion in a constrained landscape"
+# Journal: Landscape Ecology
+# DOI: 10.1007/s10980-019-00798-8
+
 remove(list=ls())
 
 ## R Libraries ##
